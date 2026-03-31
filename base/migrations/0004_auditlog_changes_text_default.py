bed0c4c04e725e3e8a0e6a0ded09e16064ea4eac
Somesh-Taiwade-AceTechnologys <somesh.acetech@nagpuranalytics.com>
2026-03-31 13:06:05 +0530

    update user

---
+++ base/migrations/0004_auditlog_changes_text_default.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("auditlog", "0017_add_actor_email"),
+        ("base", "0003_alter_dynamicemailconfiguration_options_and_more"),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="""
+                ALTER TABLE auditlog_logentry
+                ALTER COLUMN changes_text SET DEFAULT '';
+            """,
+            reverse_sql="""
+                ALTER TABLE auditlog_logentry
+                ALTER COLUMN changes_text DROP DEFAULT;
+            """,
+        ),
+    ]
